4f114e94da159f5551dc89b64f883bee4561d9d4
Timothy J. Aveni <me@timothyaveni.com>
2019-01-04 17:14:05 -0500

    Add "Show Rollover with Budget" feature
    If my goal is to budget $100/month in a category, and I only spent $70 last month, I need a quick way to see that I only need to budget $70 in the category this month to hit my target. Goals can't do this, and using the "available" column only works until I have a transaction in that category for the month. This way, I can more easily see how much money I had available at the start of the month -- it's the amount budgeted, plus last month's overflow.
    
    I based this feature on display-upcoming-amount.

---
+++ src/extension/features/budget/show-rollover-with-budget/index.js
@@ -0,0 +1,34 @@
+import { Feature } from 'toolkit/extension/features/feature';
+import { isCurrentRouteBudgetPage } from 'toolkit/extension/utils/ynab';
+import { getEmberView } from 'toolkit/extension/utils/ember';
+import { formatCurrency } from 'toolkit/extension/utils/currency';
+
+export class ShowRolloverWithBudget extends Feature {
+  injectCSS() { return require('./index.css'); }
+
+  shouldInvoke() {
+    return isCurrentRouteBudgetPage();
+  }
+
+  invoke() {
+    $('.budget-table-row.is-sub-category').each((_, element) => {
+      const { monthlySubCategoryBudgetCalculation, monthlySubCategoryBudget, subCategory } = getEmberView(element.id, 'category');
+
+      if (monthlySubCategoryBudgetCalculation && monthlySubCategoryBudgetCalculation.balancePreviousMonth) {
+        const total = monthlySubCategoryBudgetCalculation.balancePreviousMonth + monthlySubCategoryBudget.budgeted;
+        $('.budget-table-cell-budgeted', element)
+          .addClass('toolkit-budget-rollover')
+          .prepend($('<div>', {
+            class: 'toolkit-budget-rollover-amount currency',
+            title: `Total rollover from last month for ${subCategory.get('name')}. Total is ${formatCurrency(total)}.`,
+            text: formatCurrency(monthlySubCategoryBudgetCalculation.balancePreviousMonth) + ' + '
+          }));
+      }
+    });
+  }
+
+  onRouteChanged() {
+    if (!this.shouldInvoke()) return;
+    this.invoke();
+  }
+}
---
+++ src/extension/features/budget/show-rollover-with-budget/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'ShowRolloverWithBudget',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Show Rollover with Budget',
+  description: 'Adds the total of upcoming transactions alongside activity for each category.'
+};
